512e953df406f58cf9e2268ca8bfbe17d7107dbc
Diermeier <adnmby@gmail.com>
2021-05-12 17:51:49 +0200

    init

---
+++ src/Exp.Aspform.Form/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exp.Aspform.Form.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
